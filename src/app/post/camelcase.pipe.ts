f9391f5b33794d7c2fcc0d6b856b942363d26bd5
Akshithuppula <akshithuppula1234@gmail.com>
2025-01-08 15:05:34 +0530

    bugs

---
+++ src/app/post/camelcase.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'camelcase'
+})
+export class CamelcasePipe implements PipeTransform {
+
+  transform(value: string): string {
+    if (!value) return value;
+
+    return value
+      .split(' ')
+      .map((word, index) => 
+        index === 0 
+          ? word.toLowerCase() 
+          : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      )
+      .join('');
+  }
+}

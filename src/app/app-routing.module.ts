3da5ed733000e987f2f5830d1c44fd97c032339f
Akshithuppula <akshithuppula1234@gmail.com>
2025-01-08 16:32:27 +0530

    view bugs cleared

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './post/index/index.component';
+import { ViewComponent } from './post/view/view.component';
 
 const routes: Routes = [
   {path: '', component: IndexComponent },
